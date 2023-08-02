12d9d45b9820061042ab1652a21c63db75a1cc68
Elisa Edson <e.edson@protonmail.ch>
2023-08-02 18:20:29 +0100

    the amazing server response button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import axios from "axios";
 
 function App() {
+
+  async function getServerResponse(){
+    console.log("function running");
+    const API = `http://localhost:8092`
+    await axios.get(API);
+    const res = await axios.get(API);
+    console.log(res);
+    }
+
   return (
-    <div className="App">
+    <div className="App">    
+    <button onClick={()=>getServerResponse()}>The amazing server response button</button>
     </div>
   );
 }
